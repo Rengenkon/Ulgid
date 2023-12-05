b852d7091aabb6bc9a6edb1e73790b3024eafe38
Rengenkon <konnov.rinat.mail.ru@gmail.com>
2023-12-05 19:06:12 +0400

    Init

---
+++ src/main/java/ru/rengen/Ulgid/UlgidApplication.java
@@ -0,0 +1,13 @@
+package ru.rengen.Ulgid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlgidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlgidApplication.class, args);
+	}
+
+}
